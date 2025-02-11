ccbd71d3b117e0726e3a99d4ff7b732944b06b00
Danileychencko <dmitiy.dan@gmail.com>
2025-02-11 17:21:44 +0200

    added alerts, results screen, endless mode

--- lib/providers/fetched_data_provider.dart
+++ lib/providers/fetched_data_provider.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:space_scutum_test_task/models/question_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:space_scutum_test_task/providers/query_configuration_provider.dart';
@@ -19,40 +18,4 @@ final fetchedDataProvider =
     },
   ).toList();
   return questions;
-});
-
-final geminiDataProvider =
-    FutureProvider.autoDispose<List<dynamic>>((ref) async {
-  final schema = Schema.array(
-    description: 'List of questions',
-    items: Schema.object(
-      properties: {
-        'index': Schema.integer(description: 'question index, index 0 - start'),
-        'question': Schema.string(description: 'question text'),
-        'answer_index': Schema.integer(
-            description: 'index of correct answer, index 0 - start'),
-        'hint': Schema.string(description: 'hint for the user'),
-        'options': Schema.array(
-          description: 'list of answer options',
-          items: Schema.string(description: 'answer option'),
-        )
-      },
-      requiredProperties: ['index', 'question', 'answer_index', 'options'],
-    ),
-  );
-  final model = GenerativeModel(
-    model: 'gemini-2.0-flash',
-    apiKey: 'AIzaSyC97L6ewZeN_qAe7QjbuXwVMtJVDi8vM8U',
-    generationConfig: GenerationConfig(
-      responseMimeType: 'application/json',
-      responseSchema: schema,
-    ),
-  );
-
-  const promt =
-      'Кількість питань - 50, кількість варіантів - 4, тема - Фільми, складність - важка, мова - українська';
-  final content = [Content.text(promt)];
-  final response = await model.generateContent(content);
-  final List<dynamic> result = json.decode(response.text!);
-  return result;
 });
